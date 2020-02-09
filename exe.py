bf31c9ce4df5907426633580d66d351f1807f685
shulala <zj2060203064@gmail.com>
2020-02-09 12:39:26 -0600

    Added two try except blocks
    make sure users input a number when they are on the menu

--- exe.py
+++ exe.py
@@ -3,19 +3,28 @@ import re
 
 while 1:
     print("1.Admin mode\n2.Adding availability mode\n3.Quit")
-    menu_selection = int(input())
+    try:
+        menu_selection = int(input("Enter a number:"))
+    except ValueError:
+        print("Not a number.")
+        continue
     if menu_selection == 1:
         creator_name = input("Please enter your name:")
         my_admin = schedule_class.admin_mode(creator_name)
         while 1:
             print("1.Add events\n2.View your events\n3.Check availabe attendees\n4.Quit")
-            admin_selection = int(input())
+            try:
+                admin_selection = int(input())
+            except ValueError:
+                print("Not a number.")
+                continue
             if admin_selection == 1:
                 event_name = input("Please enter the event name:")
                 event_year = int(input("Please enter the year:"))
                 event_month = int(input("Please enter the month:"))
                 event_day = int(input("Please enter the day:"))
-                time_string = input("Please enter the beginning time(eg:10.30):")
+                time_string = input(
+                    "Please enter the beginning time(eg:10.30):")
                 temp = re.findall(r'\d+', time_string)
                 time = list(map(int, temp))
                 start_hour = time[0]
@@ -25,8 +34,10 @@ while 1:
                 time = list(map(int, temp))
                 end_hour = time[0]
                 end_minute = time[1]
-                event_time_slot = schedule_class.slot(start_hour, start_minute, end_hour, end_minute)
-                event_date = schedule_class.date(event_year,event_month,event_day,event_time_slot)
+                event_time_slot = schedule_class.slot(
+                    start_hour, start_minute, end_hour, end_minute)
+                event_date = schedule_class.date(
+                    event_year, event_month, event_day, event_time_slot)
                 current_event = schedule_class.event(event_name, event_date)
                 my_admin.add_event(current_event)
             elif admin_selection == 2:
@@ -34,5 +45,3 @@ while 1:
 
             elif admin_selection == 4:
                 break
-
-
