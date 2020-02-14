9eed93a0abf3ce6ff73096ab95bf0219440d2413
shulala <zj2060203064@gmail.com>
2020-02-14 17:48:05 -0600

    Reformated the structure and added some time slot function and check function

--- schedule_class.py
+++ schedule_class.py
@@ -1,34 +1,49 @@
+class user:
+    def __init__(self, name):
+        self.name = name
+        self.admin = admin_mode
+        self.attend = attend_mode
+
+
 class admin_mode:
     def __init__(self):
-        self.__events_list = []
+        self.events_list = []
         self.creator_name = ""
 
     def set_name(self, creator_name):
         self.creator_name = creator_name
 
     def add_event(self, single_event):
-        self.__events_list.append(single_event)
+        self.events_list.append(single_event)
 
     def print_all(self):
         print("Creator of events:", self.creator_name)
-        for i in range(len(self.__events_list)):
-            self.__events_list[i].print_event()
+        for i in range(len(self.events_list)):
+            self.events_list[i].print_event()
 
     def print_event_name(self):
-        for i in range(len(self.__events_list)):
-            self.__events_list[i].print_event_name()
+        for i in range(len(self.events_list)):
+            self.events_list[i].print_event_name()
 
     def check_exist(self, event_name):
-        for i in range(len(self.__events_list)):
-            if self.__events_list[i].event_name == event_name:
+        for i in range(len(self.events_list)):
+            if self.events_list[i].event_name == event_name:
                 return True
         return False
+
+    def find_event(self, event_name):
+        for i in range(len(self.events_list)):
+            if self.events_list[i].event_name == event_name:
+                return self.events_list[i]
 
 
 class event:
     def __init__(self, event_name, event_date):
         self.event_name = event_name
         self.event_date = event_date
+
+    def set_event_name(self, name):
+        self.event_name = name
 
     def print_event(self):
         print(self.event_name)
@@ -70,7 +85,7 @@ class slot:
             if self.time_slot[i] == 1:
                 print(i)
 
-    def convert_time(self):
+    def convert_time_24(self):
         for i in range(len(self.time_slot)):
             if self.time_slot[i] == 1:
                 current_time = i * 20
@@ -86,6 +101,52 @@ class slot:
                     print("{}:{}-{}:{}".format(current_hour,
                                                current_minute, current_hour, current_minute + 20))
 
+    def convert_time_12(self):
+        for i in range(0, 35):
+            if self.time_slot[i] == 1:
+                current_time = i * 20
+                current_hour = int(current_time / 60)
+                current_minute = current_time % 60
+                if current_minute == 0:
+                    print("{}:00 am-{}:{} am".format(current_hour,
+                                                     current_hour, current_minute + 20))
+                elif current_minute == 40:
+                    print("{}:{} am-{}:00 am".format(current_hour,
+                                                     current_minute, current_hour + 1))
+                else:
+                    print("{}:{} am-{}:{} am".format(current_hour,
+                                                     current_minute, current_hour, current_minute + 20))
+        if self.time_slot[35] == 1:
+            print("11:40 am-12:00 pm")
+        for i in range(36, 39):
+            current_time = i * 20
+            current_hour = int(current_time / 60)
+            current_minute = current_time % 60
+            if current_minute == 0:
+                print("{}:00 pm-{}:{} pm".format(current_hour,
+                                                 current_hour, current_minute + 20))
+            elif current_minute == 40:
+                print("{}:{} pm-{}:00 pm".format(current_hour,
+                                                 current_minute, current_hour + 1))
+            else:
+                print("{}:{} pm-{}:{} pm".format(current_hour,
+                                                 current_minute, current_hour, current_minute + 20))
+
+        for i in range(39, 72):
+            if self.time_slot[i] == 1:
+                current_time = i * 20
+                current_hour = int(current_time / 60) - 12
+                current_minute = current_time % 60
+                if current_minute == 0:
+                    print("{}:00 pm-{}:{} pm".format(current_hour,
+                                                     current_hour, current_minute + 20))
+                elif current_minute == 40:
+                    print("{}:{} pm-{}:00 pm".format(current_hour,
+                                                     current_minute, current_hour + 1))
+                else:
+                    print("{}:{} pm-{}:{} pm".format(current_hour,
+                                                     current_minute, current_hour, current_minute + 20))
+
     def print_time(self):
         if self.start_minute == 0:
             print("Beginning time: {}:00".format(self.start_hour))
@@ -97,20 +158,35 @@ class slot:
         else:
             print("End time: {}:{}".format(self.end_hour, self.end_minute))
 
+    def compare_time_slot(self, start_hour, start_minute, end_hour, end_minute):
+        is_occupied = False
+        beginning = int(3 * start_hour + start_minute / 20)
+        end = int(3 * end_hour + end_minute / 20)
+        for i in range(beginning, end):
+            if self.time_slot[i] == 1:
+                is_occupied = True
+                current_time = i * 20
+                current_hour = int(current_time / 60)
+                current_minute = current_time % 60
+                if current_minute == 0:
+                    print("{}:00-{}:{} occupied".format(current_hour,
+                                                        current_hour, current_minute + 20))
+                elif current_minute == 40:
+                    print("{}:{}-{}:00 occupied".format(current_hour,
+                                                        current_minute, current_hour + 1))
+                else:
+                    print(
+                        "{}:{}-{}:{} occupied".format(current_hour, current_minute, current_hour, current_minute + 20))
+        if not is_occupied:
+            for i in range(beginning, end):
+                self.time_slot[i] = 1
+        else:
+            print("Nothing is added.")
 
-class availability:
+
+class attend_mode:
     def __init__(self):
-        self.users_list = []
+        self.event_list = []
 
-    def add_user(self, the_user):
-        self.users_list.append(the_user)
-
-
-class user:
-    def __init__(self, name, start_hour, start_minute, end_hour, end_minute):
-        self.name = name
-        self.start_hour = start_hour
-        self.start_minute = start_minute
-        self.end_hour = end_hour
-        self.end_minute = end_minute
-        self.time_slot = [0] * 72
+    def add_event(self, the_event):
+        self.event_list.append(the_event)
